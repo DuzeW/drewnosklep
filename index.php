0cb872916170dabc337db8023b04676b3e8fe165
Wiktor <wiktor_laptop@example.com>
2023-05-19 18:35:29 +0200

    logo

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="sklep z deskami" />
+    <meta name="keywords" content="Desk" />
+    <link rel="Shortcut icon" href="img/LOGO.png"/>
+    <title>DrewnoSklep</title>
+    
