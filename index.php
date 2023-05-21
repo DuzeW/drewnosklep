b3f1121eee98002b5cc1e2ab7f2765f891d7fb94
WIktorWojcicki <wiktor.wojcicki1@gmail.com>
2023-05-21 17:32:27 +0200

    sql connected for products

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <div class="sidebar">
     <div><img src="img/drewnosklep-logo.png" alt="drewnosklep-logo" width="200" height="150"></div>
     <div><a href="index.php">Strona Główna</a></div>
-    <div><a href="products.php">Produkty</a></div>
+    <div><a href="subdomain/products.php">Produkty</a></div>
     <div>3</div>
     <button class="toggle-button" onclick="toggleSidebar()">
         <img src="img/hidden.png" alt="hidde" width="75" height="75">
