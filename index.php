2d4542803fb1c23bfbef3dfb4a2408b903a72f7b
Wiktor <wiktor_laptop@example.com>
2023-06-02 16:29:33 +0200

    sidebar in outside file

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
+
 <?php
+include 'php_function/sidebar.php'
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -13,15 +15,11 @@
     <title>DrewnoSklep</title>
 </head>
 <body>
-<div class="sidebar">
-    <div><img src="img/drewnosklep-logo.png" alt="drewnosklep-logo" width="200" height="150"></div>
-    <div><a href="index.php">Strona Główna</a></div>
-    <div><a href="subdomain/products.php">Produkty</a></div>
-    <div>3</div>
-    <button class="toggle-button" onclick="toggleSidebar()">
-        <img src="img/hidden.png" alt="hidde" width="75" height="75">
-    </button>
-</div>
+
+<?php
+sidebar();
+?>
+
 <div class="right">
 <div class="content_a">
     <center><h2>STRONA GŁÓWNA</h2></center>
